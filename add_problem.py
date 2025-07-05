e9cfa168f9c154205e198da666673e6ec2632c0d
Roman Vovchenko <vovchenko.ra@phystech.edu>
2025-07-05 20:16:15 +0300

    Problem adding improvement: task name and link automated

--- add_problem.py
+++ add_problem.py
@@ -5,7 +5,10 @@ from pathlib import Path
 
 def get_problem_num(line: str):
     dot_pos = line.find('.')
-    number = line[3:dot_pos]
+    curr_pos = dot_pos - 1
+    while curr_pos >= 0 and line[curr_pos:dot_pos].isdigit():
+        number = line[curr_pos:dot_pos]
+        curr_pos -= 1
     return number
 
 
@@ -16,37 +19,53 @@ def default_font_diff(line: str) -> str:
 def colored_diff_shields(line: str) -> str:
     if line == 'Easy':
         return '![](https://img.shields.io/badge/Easy-1cb8b8)'
-    elif line == 'Medium':
+    elif line == 'Med.':
         return '![](https://img.shields.io/badge/Medium-ffa800)'
     elif line == 'Hard':
         return '![](https://img.shields.io/badge/Hard-f63636)'
 
 
-def add_to_readme(num: str, dif: str):
-    with open('README.md', 'r+', encoding='utf-8') as file:
-        insert_string = f'| [{num}. ]() '
-        if dif == 'e':
-            insert_string += ('| [😎](https://github.com/r-vvch/LeetCode/blob/main/solutions/'
-                              + num + '_.py) | `` | ' + colored_diff_shields('Easy') + '|\n')
-        elif dif == 'm':
-            insert_string += ('| [🤔](https://github.com/r-vvch/LeetCode/blob/main/solutions/'
-                              + num + '_.py) | `` | ' + colored_diff_shields('Medium') + '|\n')
-        elif dif == 'h':
-            insert_string += ('| [😤](https://github.com/r-vvch/LeetCode/blob/main/solutions/'
-                              + num + '_.py) | `` | ' + colored_diff_shields('Hard') + '|\n')
-        contents = file.readlines()
-
-        if int(num) > int(get_problem_num(contents[-1])):
-            contents.append(insert_string)
+def add_to_readme(num: str):
+    with open('problems_list.txt', 'r', encoding='utf-8') as file:
+        problems = file.readlines()
+        if int(num) > int(get_problem_num(problems[-3])):
+            insert_string = (
+                f'| [{num}. ]() ' + 
+                '| [](https://github.com/r-vvch/LeetCode/blob/main/solutions/' +
+                num + '_.py) | `` |  |\n')
         else:
-            for l_num in range(6, len(contents) - 1):
-                if (int(get_problem_num(contents[l_num])) < int(num) and
-                    int(get_problem_num(contents[l_num + 1])) > int(num)):
-                    contents.insert(l_num + 1, insert_string)
+            for l_num in range(int(num) * 4 - 4, min(int(num) * 4, len(problems))):
+                if int(get_problem_num(problems[l_num])) == int(num):
+                    problem_name = problems[l_num][:-1]
+                    dot_pos = problem_name.find('.')
+                    problem_name_no_num = problem_name[dot_pos + 2:]
+                    problem_diff = problems[l_num + 2][:-1]
+                    insert_string = (
+                        f'| [{problem_name}](https://leetcode.com/problems/' +
+                        f'{problem_name_no_num.lower().replace(' ', '-')}/) ' +
+                        '| [](https://github.com/r-vvch/LeetCode/blob/main/solutions/' +
+                        problem_name.lower().replace(' ', '_').replace('.', '') + '.py) ' +
+                        '| `` ' +
+                        '| ' + colored_diff_shields(problem_diff) + ' '
+                        '|\n')
+                    break
+
+    with open('README.md', 'r+', encoding='utf-8') as file:
+        contents = file.readlines()
+        contents_alg = contents[:-6]
+        contents_sql = contents[-6:]
+        if int(num) > int(get_problem_num(contents_alg[-1])):
+            contents_alg.append(insert_string)
+        else:
+            for l_num in range(6, len(contents_alg) - 1):
+                if (int(get_problem_num(contents_alg[l_num])) < int(num) and
+                    int(get_problem_num(contents_alg[l_num + 1])) > int(num)):
+                    contents_alg.insert(l_num + 1, insert_string)
                     break
 
         file.seek(0)
-        file.writelines(contents)
+        full_contents = contents_alg + contents_sql
+        file.writelines(full_contents)
 
 
 def add_py_file(num: str):
@@ -62,7 +81,6 @@ def add_py_file(num: str):
 
 if __name__ == '__main__':
     problem_num = sys.argv[1]
-    problem_dif = sys.argv[2]
 
-    add_to_readme(problem_num, problem_dif)
+    add_to_readme(problem_num)
     add_py_file(problem_num)
